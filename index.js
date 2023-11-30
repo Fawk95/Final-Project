c1eddee17d0e1559a72914c7974cc501e6751b91
Fawk95 <151018343+Fawk95@users.noreply.github.com>
2023-11-30 19:33:56 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = searchInput.value;
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
 const form = document.querySelector("form");
 const input = document.querySelector('input[type="text"]');
 
@@ -24,3 +33,34 @@ function showUser(response) {
 
 let url = "https://jsonplaceholder.typicode.com/users/1";
 axios.get(url).then(showUser);
+
+function searchCity(city) {
+  let forecast = document.querySelector("#forecast");
+  let apiKey = "ebfa973675011tfoc4a6499daf9c3c06";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key={key}`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+forecast.innerhtml = `
+  <div class="weather-forecast-day">
+    <div class="weather-forecast-date">Tues</div>
+    <div class="weather-forcast-icon"></div>
+    <div class="weather-forecast-temperature">
+      <div class="weather-forecast-temperature">
+        <strong>32</strong>
+      </div>
+      <div class="weather-forcast-temperature">28</div>
+    </div>
+  </div>
+`;
+getForecast(response.data.city);
+
+function getForecast(city) {
+  let apiKey = "ebfa973675011tfoc4a6499daf9c3c06";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key={key}`;
+  axios(apiUrl).then(displayForecast);
+}
+
+function displayForecast(response) {
+  console.log(response.data);
+}
+searchCity("Perth");
